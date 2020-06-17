dd5c02b8ccf3a175ccbc0bf81ec7b50110b2d297
zhangkang123 <thewinteriscoming@163.com>
2020-06-17 18:02:47 +0800

    🐞fix: 新闻列表

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,23 @@
   <div class="about">
     <h1>This is an about page</h1>
     <van-button type="info">信息按钮</van-button>
+   <div v-for="item in list" :key="item.uniquekey">
+     <a :href="item.url">
+       <div>{{item.title}}</div>
+     <img :src="item.thumbnail_pic_s" >
+     </a>
+   </div>
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      list:''
+    }
+  },
   mounted() {
-    // console.log(this.$utils.getDate());
-    // console.log(this.$regex);
     this.query();
   },
   methods: {
@@ -19,7 +28,9 @@ export default {
       });
       const { error_code, reason, result } = res;
       if (error_code !== 0) return this.$toast.fail(reason);
-      console.log(result);
+      this.list=result.data
+      console.log(this.list);
+      
     },
   },
 };
